5815ca7d657b31f94bce58efb8abe9a6a2f3e5e4
Shola Ayeni <ayenisholah@yahoo.com>
2020-04-20 20:51:46 +0100

    implemented get single author action method

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace CourseLibrary.API.Controllers
 {
     [ApiController]
+    [Route("api/authors")]
     public class AuthorsController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
@@ -15,11 +16,23 @@ namespace CourseLibrary.API.Controllers
                 throw new ArgumentNullException(nameof(courseLibraryRepository));
         }
 
-        [HttpGet("api/authors")]
+        [HttpGet()]
         public IActionResult GetAuthors()
         {
             var authors = _courseLibraryRepository.GetAuthors();
             return new JsonResult(authors);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var author = _courseLibraryRepository.GetAuthor(authorId);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return Ok(author);
         }
     }
 }
